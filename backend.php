18cbee7f5a0e0d255592872ce3b064b372c7cc1a
ankyspanky0508 <Ankur@ankur-ohris-macbook-pro.local>
2014-07-10 17:47:47 -0400

    Well all the files are updates and connected to the data base. still dont have the hash for the password but bedies that we are golden

---
+++ backend.php
@@ -0,0 +1,41 @@
+<?php
+
+// Connecting to the database
+mysql_connect("sql.njit.edu","ako6","Headset1");
+mysql_select_db("ako6");
+
+ob_start();
+$host="localhost"; // Host name 
+$username=""; // Mysql username 
+$password=""; // Mysql password 
+$db_name="ako6"; // Database name 
+$table_name="490project"; // Table name 
+
+// Define $myusername and $mypassword 
+$myusername=$_POST['myusername']; 
+$mypassword=$_POST['mypassword']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $table_name WHERE username='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("myusername");
+session_register("mypassword"); 
+header("location:login.php");
+}
+else {
+echo "Wrong Username or Password";
+}
+ob_end_flush();
+?>
