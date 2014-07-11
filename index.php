18dcca5491ce6701d83a78d52b93ed881ffba6d5
Raj <rajkothari24@gmail.com>
2014-07-11 17:38:00 -0400

    front view complete... still need ajax

--- index.php
+++ index.php
@@ -3,22 +3,28 @@
     <link rel="stylesheet" type="text/css" href="css/custom.css">
     <link rel="stylesheet" type="text/css" href="css/foundation.css">
   </header>
-    
-  <div id="head">
-    CS 101 ExamLocation
-  </div>
-  <br/>
-  <br/>
-  <br/>
-  <div class="row"> 
-    <div class="small-4 small-centered columns">
-      <form action="middle.php" method="post">
-          User Name:
-          <input type="text" name="username">
-          Password:
-          <input type="password" name="password">
-          <button class="right" type="submit" name="submit" value="Login">Login</button>
-      </form>
+  
+  <body class="blackboard"> 
+    <div id="head">
+      CS 101 ExamLocation
     </div>
-  </div>
+    <br/>
+    <br/>
+    <br/>
+    <div class="row"> 
+      <div class="small-4 small-centered columns">
+        <div>
+        <form action="middle.php" method="post">
+            <h5 class="white">User Name:</h5>
+            <input type="text" name="user">
+            <h5 class="white">Password:</h5>
+            <input type="password" name="pass">
+            <button class="right" id="loginstyle" type="submit" name="submit" value="Login">Login</button>
+        </form>
+      <!-- <?php echo $_POST['username']; ?>
+       <?php echo $_POST['password']; ?> -->
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
