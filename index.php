8ebf2c09d41aa93bcd325a04185500a021bcebc9
ankyspanky0508 <Ankur@ankur-ohris-macbook-pro.local>
2014-07-12 17:12:30 -0400

    back to original files updated db

--- index.php
+++ index.php
@@ -1,81 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="jquery.js"></script>
-<link rel="stylesheet" href="styles.css" type="text/css" />
-<title>Login Form</title>
+<html>
+
 <script type="text/javascript">
-$(document).ready(function(){
-	
-   $("#login").click(function(){
-		username=$("#user_name").val();
-        password=$("#password").val();
-
-         $.ajax({
-            type: "POST",
-           url: "login.php",
-            data: "username="+username+"&password="+password,
-
-
-            success: function(html){
-			
-			  if(html=='true')
-              {
-                $("#login_form").fadeOut("normal");
-				$("#shadow").fadeOut();
-				$("#profile").html("<a href='logout.php' class='red' id='logout'>Logout</a>");
-				// You can redirect to other page here....
-              }
-              else
-              {
-                    $("#add_err").html("Wrong username or password");
-              }
-            },
-            beforeSend:function()
-			{
-                 $("#add_err").html("Loading...")
-            }
-        });
-         return false;
-    });
-});
 </script>
-</head>
-<body>
-<?php session_start(); ?>
-	<div id="profile">
-     	<?php
-   	if(isset($_SESSION['user_name'])){
-			?>
-	<a href='logout.php'>Logout</a>
-		<?php } ?>
-	</div>
-</body>
-<?php 
-if(empty($_SESSION['user_name'])){?>
-<div class="container" id="login_form">
-	<section id="content">
-		<form action="login.php">
-			<h1>Login Form</h1>
-			<div>
-				<input type="text" placeholder="Username" required="" id="user_name"  name="user_name"/>
-			</div>
-			<div>
-				<input type="password" placeholder="Password" required="" id="password"  name="password"/>
-			</div>
-		        <div class="err" id="add_err"></div>
-			<div>
-					<input type="submit" value="Log in" id="login"  />
-				<a href="#">Lost your password?</a>
-				<a href="#">Register</a>
-			</div>
-		</form><!-- form -->
-		<div class="button">
-			
-		</div><!-- button -->
-	</section><!-- content -->
-</div>
-<?php }?>
-<!-- container -->
+
+  <header>
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/foundation.css">
+  </header>
+  
+  <body class="blackboard"> 
+    <div id="head">
+      CS 101 ExamLocation
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <div class="row"> 
+      <div class="small-4 small-centered columns">
+        <div>
+        <form id="login" action="backend.php" method="post">
+            <h5 class="white">User Name:</h5>
+            <input type="text" name="user">
+            <h5 class="white">Password:</h5>
+            <input type="password" name="pass">
+            <button class="right" id="loginstyle" type="submit" name="submit" value="Login">Login</button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
--- login.php
+++ login.php
@@ -1,28 +1,11 @@
 <?php
 session_start();
-
-$username = $_POST['username'];
-$password = md5($_POST['password']);
-
-$mysql_db_hostname = "sql.njit.edu";
-$mysql_db_user = "ako6";
-$mysql_db_password = "admin";
-$mysql_db_database = "ako6";
-
-$con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password) or die("Could not connect database");
-mysql_select_db($mysql_db_database, $con) or die("Could not select database");
-
-$query = "SELECT * FROM registered_users WHERE name='$username' AND password='$password'";
-$result = mysql_query($query)or die(mysql_error());
-$num_row = mysql_num_rows($result);
-		$row=mysql_fetch_array($result);
-		if( $num_row >=1 ) {
-
-			echo 'true';
-			$_SESSION['user_name']=$row['name'];			
-			
-		}
-		else{
-			echo 'false';
-		}
+if(!session_is_registered(username)){
+header("location:index.php");
+}
 ?>
+<html>
+<body>
+YOUUUU ARE INNNN BBEEEEEEEETCH
+</body>
+</html>
