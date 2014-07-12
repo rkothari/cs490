41de34aa9d4051d0cfd5d231120db153c0d460e5
ankyspanky0508 <Ankur@ankur-ohris-macbook-pro.local>
2014-07-12 15:30:57 -0400

    updated db

--- inc/dbConnect.inc.php
+++ inc/dbConnect.inc.php
@@ -5,10 +5,10 @@ $mysql_db_password = "Password";
 $mysql_db_database = "Database Name";*/
 
 $mysql_db_hostname = "localhost";
-$mysql_db_user = "Root";
+$mysql_db_user = "";
 $mysql_db_password = "";
-$mysql_db_database = "demo";
+$mysql_db_database = "490project";
 
-$con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password) or die("Could not connect database");
-mysql_select_db($mysql_db_database, $con) or die("Could not select database");
+$con = mysql_connect("sql.njit.edu","ako6","Headset1"); or die("Could not connect database");
+mysql_select_db("ako6", $con); or die("Could not select database");
 ?>
