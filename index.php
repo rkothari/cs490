ebe1c0ad762dd97bb45b49aa86b4223ba4a6fa70
ankyspanky0508 <Ankur@ankur-ohris-macbook-pro.local>
2014-07-12 20:23:00 -0400

    updated project

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
-<html>
-	
-	<script type="text/javascript">
-	</script>
-	
-	<header>
-		<link rel="stylesheet" type="text/css" href="css/custom.css">
-		<link rel="stylesheet" type="text/css" href="css/foundation.css">
-	</header>
+<?php
+  session_start();
+  if(!isset($_SESSION['LOGIN_STATUS'])){
+      header('location:login.php');
+  }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP Ajax Login Validation Tutorial | 91 Web Lessons</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="container">
+    <!--top section start-->
+    <div id='top'>
+         <a href="http://www.91weblessons.com" title="91 Web Lessons" target="blank">
+             <img src="image/logo.png" alt="91 Web Lessons" title="91 Web Lessons" border="0"/>
+         </a>
+    </div>
 
-	<head>
-		<title>CS490 Project</title>
-	</head>
-	<body class="blackboard">
-		<div id="head">
-			CS 101 Examlocation
-		</div>
-		<br />
-		<br />
-		<br />
-		<div class="row">
-			<div class="small-4 small-centered columns">
-				
-				<div>
-					<form id="login" action="backend.php" method="POST">
-						<h5 class="white">User Name: </h5>
-						<input type="text" name="user">
-						<h5 class="white">Password: </h5>
-						<input type="password" name="pass">
-						<button class="right" id="loginstyle" type="submit" name="submit" value="login">Login</button>
-					</form>
-				</div>
+    <div id='tutorialHead'>
+         <div class="tutorialTitle"><h1>PHP Ajax Login Validation Tutorial</h1></div>
+         <div class="tutorialLink"><a href="http://www.91weblessons.com/php-ajax-login-validation-tutorial" title="PHP Ajax Login Validation Tutorial"><h1>Tutorial Link</h1></a></div>
 
-			</div>
-		</div>
-	
-	</body>
+         <div class="logout"><a href="logout.php" title="logout"><h1>Logout</h1></a></div>
+
+    </div>
+
+    <div id="wrapper">
+         <div class="user_intro"><h1>Welcome <?php echo $_SESSION['UNAME'];?></h1></div>
+    </div>
+
+    <!--fotter section start-->
+    <div id="fotter">
+         <p>Copyright &copy; 2012 
+              <a href="http://www.91weblessons.com" title="91 Web Lessons" target="blank">91 Web Lessons</a>. 
+              All rights reserved.
+         </p>
+    </div>
+</div>
+</body>
 </html>
